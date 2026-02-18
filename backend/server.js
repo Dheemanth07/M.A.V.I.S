83cbff910a61407826e9bc9ad383b8afde620c2c
Dheemanth07 <dheemanth1007@gmail.com>
2026-02-18 14:31:47 +0530

    initialized backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("MAVIS backend running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
